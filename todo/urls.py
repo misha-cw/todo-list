81a230fd1bf396c78980473b20e5602974e48aa7
misha-cw <bigun.mykhaylo@gmail.com>
2025-10-29 13:00:23 +0200

    Add TagListView and tag list template; update sidebar navigation

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from todo.views import (
     TaskListView,
+    TagListView,
 )
 
 
@@ -9,4 +10,5 @@ app_name = "todo"
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="home"),
+    path("tags/", TagListView.as_view(), name="tag_list"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -9,3 +9,9 @@ class TaskListView(generic.ListView):
     context_object_name = "tasks"
     queryset = Task.objects.prefetch_related("tags").all().order_by("is_done", "-created_at")
 
+class TagListView(generic.ListView):
+    model = Tag
+    template_name = "todo/tag_list.html"
+    context_object_name = "tags"
+    queryset = Tag.objects.all().order_by("name")
+
