faf532e455383a3d41cbd9a956cdda0cc0914775
misha-cw <bigun.mykhaylo@gmail.com>
2025-10-29 13:33:25 +0200

    Add task deletion functionality with confirmation view and URL routing

--- todo/views.py
+++ todo/views.py
@@ -27,6 +27,12 @@ class TaskUpdateView(generic.UpdateView):
     success_url = reverse_lazy("todo:home")
 
 
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    template_name = "todo/task_confirm_delete.html"
+    success_url = reverse_lazy("todo:home")
+
+
 class TagListView(generic.ListView):
     model = Tag
     template_name = "todo/tag_list.html"
